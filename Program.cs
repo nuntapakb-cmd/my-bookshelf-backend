daae6ec6cbb171b5420cadc3f5ed43e0cbea497d
Nuntapak Booddee <nuntapkb-cmd@users.noreply.github.com>
2025-12-07 02:49:17 +0100

    Auto migrate database

--- Program.cs
+++ Program.cs
@@ -84,4 +84,11 @@ app.MapControllers();
 
 app.MapGet("/", () => Results.Redirect("/swagger/index.html"));
 
+// >>> auto-migrate database on startup
+using(var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate(); 
+}
+
 app.Run();
